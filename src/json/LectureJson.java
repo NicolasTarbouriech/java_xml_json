d67e3d0a52affb3582c124dfee8969cd53ae328e
Nicolas Tarbouriech <65289313+NicolasTarbouriech@users.noreply.github.com>
2020-12-15 15:05:58 +0100

    Edit apercu console plus propre

--- src/json/LectureJson.java
+++ src/json/LectureJson.java
@@ -14,27 +14,35 @@ public class LectureJson {
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 
 		ObjectMapper mapper = new ObjectMapper();
-		
+
 		CollectionType collType = mapper.getTypeFactory().constructCollectionType(List.class, Societe.class);
 		List<Societe> persons = mapper.readValue(new File("C:/Users/tarbo/Desktop/jsontest.txt"), collType);
-		
+
 		for (Societe p : persons) {
-			System.out.println("L'entreprise que l'on va voir est : "  + p.getNom());
-		
+			System.out.println("L'entreprise que l'on va voir est : " + p.getNom());
+			System.out.println("");
 			System.out.println("le siege social est : " + p.getSiegeSocial());
-			
-			for(Fondateur f : p.getFondateur()) {
+			System.out.println("");
+			for (Fondateur f : p.getFondateur()) {
 				System.out.println("le nom du pays du fondateur est : " + f.getPays());
+				System.out.println("");
 				System.out.println("la date de naissance du fondateur est : " + f.getDateDeNaissance());
+				System.out.println("");
 				System.out.println("le nom du lieu du fondateur est  " + f.getNom());
+				System.out.println("");
 				System.out.println("le lieu est : " + f.getLieu());
+				System.out.println("");
+				System.out.println("");
 			}
-			
-			for(ChiffreDaffaires ca : p.getChiffreDaffaires()) {
+			System.out.println("");
+			for (ChiffreDaffaires ca : p.getChiffreDaffaires()) {
 				System.out.println("L'annee de la valeur du Chiffre d'affaires est en : " + ca.getAnnee());
-				System.out.println("La valeur du Chiffres d'affaires cette année est de : " + ca.getValeur());
+				System.out.println("");
+				System.out.println("La valeur du Chiffres d'affaires cette année est de : " + ca.getValeur()
+						+ " milliards d'euros.");
+				System.out.println("");
 			}
-			
+			System.out.println("--------------------------------------------");
 		}
 	}
 
